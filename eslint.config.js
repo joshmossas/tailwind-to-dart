14bb7c697f95e705205b4e7fed8464538bce52a4
Joshua Sosso <josh@modiimedia.com>
2024-06-08 00:07:29 -0500

    update eslint stuff

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import unjs from "eslint-config-unjs";
+import prettier from "eslint-config-prettier";
+export default unjs(
+    {
+        rules: {
+            "unicorn/no-null": 0,
+        },
+    },
+    prettier,
+);
